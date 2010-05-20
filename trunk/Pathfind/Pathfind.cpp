d1c72be3d54f90a86ecedd27681b322c9880210a
voodoohaust <voodoohaust@97c0069c-804f-11de-81da-11cc53ed4329>
2010-05-20 15:27:10 +0000

    [PATHFIND] : started path find project.

---
+++ trunk/Pathfind/Pathfind.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+#include "../Pathfind/Pathfind.h"
+
+namespace ElixirEngine
+{
+	//-----------------------------------------------------------------------------------------------
+	//-----------------------------------------------------------------------------------------------
+	//-----------------------------------------------------------------------------------------------
+
+	AStar::AStar()
+	:	CoreObject()
+	{
+
+	}
+
+	AStar::~AStar()
+	{
+
+	}
+
+	bool AStar::Create(const boost::any& _rConfig)
+	{
+		bool bResult = false;
+		return bResult;
+	}
+
+	void AStar::Update()
+	{
+
+	}
+
+	void AStar::Release()
+	{
+
+	}
+}
