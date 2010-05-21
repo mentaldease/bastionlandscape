c8008384cf4e790400f9979b5818a5a3806bd1af
voodoohaust <voodoohaust@97c0069c-804f-11de-81da-11cc53ed4329>
2010-05-21 15:00:07 +0000

    [ASTAR] : renamed Pathfind project to AStar.

--- Pathfind/AStarTypes.h
+++ Pathfind/AStarTypes.h
@@ -1,5 +1,5 @@
-#ifndef __PATHFINDTYPES_H__
-#define __PATHFINDTYPES_H__
+#ifndef __ASTARTYPES_H__
+#define __ASTARTYPES_H__
 
 namespace ElixirEngine
 {
@@ -7,9 +7,18 @@ namespace ElixirEngine
 	//-----------------------------------------------------------------------------------------------
 	//-----------------------------------------------------------------------------------------------
 
+	class AStarNode;
+	typedef AStarNode* AStarNodePtr;
+	typedef AStarNode& AStarNodeRef;
+	typedef vector<AStarNodePtr> AStarNodePtrVec;
+
+	class AStarNodeContainer;
+	typedef AStarNodeContainer* AStarNodeContainerPtr;
+	typedef AStarNodeContainer& AStarNodeContainerRef;
+
 	class AStar;
 	typedef AStar* AStarPtr;
 	typedef AStar& AStarRef;
 }
 
-#endif // __PATHFINDTYPES_H__
+#endif // __ASTARTYPES_H__
